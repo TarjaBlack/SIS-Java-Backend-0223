b91f02db6aef90d5b9fde3734c4f111bfcee58cc
Marcelo Gonçalves de Souza <mgoncalves@digitalhouse.com>
2023-02-07 21:09:32 -0300

    Update

---
+++ 02-ClasseScanner/ClasseScanner/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int n1, n2, adicao, subtracao, mult, div;
+
+        // Atribuição de um objeto Scanner na variável sc;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o valor de n1: ");
+        n1 = sc.nextInt();
+        //System.out.println("N1: " + n1);
+
+        System.out.print("Digite o valor de n2: ");
+        n2 = sc.nextInt();
+        //System.out.println("N2: " + n2);
+
+        adicao = n1 + n2;
+        System.out.printf("O resultado da adição é: " + adicao);
+
+    }
+}
