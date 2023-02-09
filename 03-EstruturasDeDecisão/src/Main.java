3d85eba03b8c9fc24dd48c38505a6711fab4067e
Marcelo Gonçalves de Souza <mgoncalves@digitalhouse.com>
2023-02-09 19:59:12 -0300

    Update

--- 03-EstruturasDeDecisão/src/Main.java
+++ 03-EstruturasDeDecisão/src/Main.java
@@ -28,13 +28,48 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.print("Digite o salário: ");
         sal = sc.nextDouble();
-        System.out.println("Quantos dependentes: ");
+        System.out.print("Quantos dependentes: ");
         dependentes = sc.nextInt();
 
-        // Pegar a tabela do IR 2022/23 e montar uma estrutura com If/ELSE
-        // Se o contribuinte tiver 2 dependentes, se diminui 2x deducaoPorDep
+//        // 1650.52
+//        if (sal <= 1903.98) {
+//            System.out.println("Contribuinte isento de IR.");
+//        }
+//        // 2100.50
+//        if (sal >= 1903.99 && sal <= 2826.65) {
+//            System.out.printf("Contribuinte com salário de R$ %.2f pagará 7.5%% de IR.%n", sal);
+//            System.out.printf("Com dedução de %d dependentes", dependentes);
+//        }
+//        // 3550.20
+//        if (sal >= 2826.66 && sal <= 3751.05) {
+//            System.out.printf("Contribuinte com salário de R$ %.2f pagará 15%% de IR.%n", sal);
+//            System.out.printf("Com dedução de %d dependentes", dependentes);
+//        }
+//        // 3955.00
+//        if (sal >= 3751.06 && sal <= 4664.68) {
+//            System.out.printf("Contribuinte com salário de R$ %.2f pagará 22.5%% de IR.%n", sal);
+//            System.out.printf("Com dedução de %d dependentes", dependentes);
+//        }
+//        // 5410.00
+//        if (sal >= 4664.69) {
+//            System.out.printf("Contribuinte com salário de R$ %.2f pagará 27.5%% de IR.%n", sal);
+//            System.out.printf("Com dedução de %d dependentes", dependentes);
+//        }
 
+        // IF aninhado
+        if (sal <= 1903.98) {
+            System.out.println("Contribuinte isento de IR.");
+        } else if (sal >= 1903.99 && sal <= 2826.65) {
+            System.out.printf("Contribuinte com salário de R$ %.2f pagará 7.5%% de IR.%n", sal);
+        } else if (sal >= 2826.66 && sal <= 3751.05) {
+            System.out.printf("Contribuinte com salário de R$ %.2f pagará 15%% de IR.%n", sal);
+        } else if (sal >= 3751.06 && sal <= 4664.68) {
+            System.out.printf("Contribuinte com salário de R$ %.2f pagará 22.5%% de IR.%n", sal);
+        } else {
+            System.out.printf("Contribuinte com salário de R$ %.2f pagará 27.5%% de IR.%n", sal);
+        }
+
+        System.out.printf("Com direito a dedução de %d dependentes.", dependentes);
 
     }
-
 }
