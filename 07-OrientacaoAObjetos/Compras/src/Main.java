5e525a8a4fb1932c2a5186d94dab1a958fd72761
Marcelo Gonçalves de Souza <mgoncalves@digitalhouse.com>
2023-02-16 19:52:39 -0300

    Update

--- 07-OrientacaoAObjetos/Compras/src/Main.java
+++ 07-OrientacaoAObjetos/Compras/src/Main.java
@@ -10,14 +10,26 @@ public class Main {
         carrinho.setValor(699.39);
         carrinho.setPromocao(false);
 
+        Carrinho carrinho2 = new Carrinho();
+        carrinho2.setId(1);
+        carrinho2.setItem("Impressora Samsung");
+        carrinho2.setQuantidade(2);
+        carrinho2.setValor(699.39);
+        carrinho2.setPromocao(false);
+
         // Chamando o método sem parãmetros
-        System.out.println("Valor total do item: " + carrinho.calcularTotalItem());
-        System.out.println(carrinho);
+//        System.out.println("Valor total do item: " + carrinho.calcularTotalItem());
+//        System.out.println(carrinho);
 
         // Chamando o método com parâmetros
-        System.out.println("Valor total do item modificado: " + carrinho.alterarValorItem(29.20));
-        System.out.println("Valor total do item: " + carrinho.calcularTotalItem());
-        System.out.println(carrinho);
+//        System.out.println("Valor total do item modificado: " + carrinho.alterarValorItem(29.20));
+//        System.out.println("Valor total do item: " + carrinho.calcularTotalItem());
+//        System.out.println(carrinho);
 
+//        System.out.println(carrinho.getItem().hashCode());
+//        System.out.println(carrinho2.getItem().hashCode());
+
+        Carrinho carrinho3 = null;
+        System.out.println(carrinho3.getItem());
     }
 }
--- 07-OrientacaoAObjetos/Compras/src/entities/Carrinho.java
+++ 07-OrientacaoAObjetos/Compras/src/entities/Carrinho.java
@@ -1,5 +1,7 @@
 package entities;
 
+import java.util.Objects;
+
 public class Carrinho {
 
     // Atributos
@@ -72,6 +74,20 @@ public class Carrinho {
     public double alterarValorItem(double novoValor) {
         setValor(novoValor);
         return getValor();
+    }
+
+    // Equals e hashcode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Carrinho carrinho = (Carrinho) o;
+        return id == carrinho.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 
     @Override
