3fce143fbd58c4b3a9abe59e3502e6bdfa8b3f0a
Marcelo Gonçalves de Souza <mgoncalves@digitalhouse.com>
2023-03-03 19:48:58 -0300

    Update

--- 13-Coleções/Exe02-colecoes/src/Main.java
+++ 13-Coleções/Exe02-colecoes/src/Main.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,16 +12,58 @@ public class Main {
         ourList.add("Juliana");
         ourList.add("Ricardo");
 
-        System.out.println("List size: " + ourList.size());
+//        System.out.println(ourList);
 
-        System.out.println("Accessing the list with FOR:");
-        for (int i = 0; i < ourList.size(); i++) {
-            System.out.println(ourList.get(i));
-        }
+//        System.out.println("List size: " + ourList.size());
 
-        System.out.println("Accessing the list with FOR - desc:");
-        for (int i = ourList.size() - 1; i >= 0; i--) {
-            System.out.println(ourList.get(i));
+//        System.out.println("Accessing the list with FOR:");
+//        for (int i = 0; i < ourList.size(); i++) {
+//            System.out.println(ourList.get(i));
+//        }
+//
+//        System.out.println("Accessing the list with FOR - desc:");
+//        for (int i = ourList.size() - 1; i >= 0; i--) {
+//            System.out.println(ourList.get(i));
+//        }
+
+//        System.out.println("Accessing the list with ITERATOR:");
+//        Iterator it = ourList.iterator();
+//        while (it.hasNext()) {
+//            System.out.println(it.next());
+//        }
+
+//        for (Object obj : ourList) {
+////            System.out.println(obj);
+////        }
+
+        // Interface Set e a classe HashSet
+//        Set ourSet = new HashSet();
+//        ourSet.add("Brasil");
+//        ourSet.add("Argentina");
+//        ourSet.add("Uruguai");
+//        ourSet.add("Equador");
+//        ourSet.add("Suriname");
+//        ourSet.add("Uruguai");
+//
+//        System.out.println("Accessing the SET with FOREACH");
+//        for (Object obj : ourSet) {
+//            System.out.println(obj);
+//        }
+
+        // Interface Map e a classe HashMap
+        Map categories = new HashMap();
+        categories.put(1, "Desktops");
+        categories.put(2, "Notebooks");
+        categories.put(3, "Tablets");
+        categories.put(4, "Celulares");
+        categories.put(5, "Acessórios");
+
+        System.out.println("Search for key three:");
+        System.out.println(categories.get(3));
+
+        System.out.println("Accessing the MAP with FOREACH:");
+        for (Object obj : categories.values()) {
+            System.out.println(obj);
         }
 
     }
---
+++ 14-Lista04/Lista04/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // List
+
+        // Set
+
+        // Map
+
+    }
+
+    /*
+    * Equador, Holanda, Catar, Senegal, Inglaterra, Irã, Estados Unidos, País de Gales, Argentina, México, Polônia, Arábia Saudita
+Austrália, Dinamarca, França, Tunísia, Costa Rica, Alemanha, Japão, Espanha, Bélgica, Canadá, Croácia, Marrocos, Brasil
+Camarões, Sérvia, Suíça, Gana, Portugal, Coreia do Sul, Uruguai*/
+}
