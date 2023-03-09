cf4593ec6eb6c6a23c41cc42ba6b65c94a8f2d91
Marcelo Gonçalves de Souza <mgoncalves@digitalhouse.com>
2023-03-08 21:15:13 -0300

    Update

---
+++ 20-musica/src/main/java/br/com/sisnema/musica/entities/Artista.java
@@ -0,0 +1,65 @@
+package br.com.sisnema.musica.entities;
+
+import java.util.Objects;
+
+public class Artista {
+
+    private Long id;
+    private String nome;
+    private boolean banda;
+
+    public Artista() {
+    }
+
+    public Artista(Long id, String nome, boolean banda) {
+        this.id = id;
+        this.nome = nome;
+        this.banda = banda;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public boolean isBanda() {
+        return banda;
+    }
+
+    public void setBanda(boolean banda) {
+        this.banda = banda;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Artista artista = (Artista) o;
+        return Objects.equals(id, artista.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Artista{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", banda=" + banda +
+                '}';
+    }
+}
