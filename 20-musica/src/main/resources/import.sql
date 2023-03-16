ef35dc59a2c59273078ca3b49e37a072a17a893f
Marcelo Gonçalves de Souza <mgoncalves@digitalhouse.com>
2023-03-16 19:34:02 -0300

    Update

--- 20-musica/src/main/resources/import.sql
+++ 20-musica/src/main/resources/import.sql
@@ -8,3 +8,15 @@ INSERT INTO tb_artista (nome, banda, pais_id) VALUES ('Metallica', true, 1);
 INSERT INTO tb_artista (nome, banda, pais_id) VALUES ('Iron Maiden', true, 3);
 INSERT INTO tb_artista (nome, banda, pais_id) VALUES ('Pearl Jam', true, 1);
 INSERT INTO tb_artista (nome, banda, pais_id) VALUES ('Molotov', true, 5);
+
+INSERT INTO tb_album (titulo, ano, artista_id) VALUES ('Kill ''Em All', 1983, 1);
+INSERT INTO tb_album (titulo, ano, artista_id) VALUES ('Ride The Lightning', 1984, 1);
+INSERT INTO tb_album (titulo, ano, artista_id) VALUES ('Master Of Puppets', 1986, 1);
+INSERT INTO tb_album (titulo, ano, artista_id) VALUES ('... And Justice For All', 1988, 1);
+INSERT INTO tb_album (titulo, ano, artista_id) VALUES ('Metallica', 1991, 1);
+INSERT INTO tb_album (titulo, ano, artista_id) VALUES ('Load', 1996, 1);
+INSERT INTO tb_album (titulo, ano, artista_id) VALUES ('ReLoad', 1997, 1);
+INSERT INTO tb_album (titulo, ano, artista_id) VALUES ('Garage Inc', 1998, 1);
+INSERT INTO tb_album (titulo, ano, artista_id) VALUES ('St. Anger', 2003, 1);
+INSERT INTO tb_album (titulo, ano, artista_id) VALUES ('Death Magnetic', 2008, 1);
+INSERT INTO tb_album (titulo, ano, artista_id) VALUES ('Hardwired--- to Self-Desctruct', 2016, 1);
