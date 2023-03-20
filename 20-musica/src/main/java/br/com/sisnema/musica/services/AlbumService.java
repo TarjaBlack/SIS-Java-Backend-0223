e25e934bc73da67b2810e5eece1f5774be23a823
Marcelo Gonçalves de Souza <mgoncalves@digitalhouse.com>
2023-03-20 20:57:51 -0300

    Update

--- 20-musica/src/main/java/br/com/sisnema/musica/services/AlbumService.java
+++ 20-musica/src/main/java/br/com/sisnema/musica/services/AlbumService.java
@@ -4,10 +4,13 @@ import br.com.sisnema.musica.dtos.AlbumDto;
 import br.com.sisnema.musica.entities.Album;
 import br.com.sisnema.musica.entities.Artista;
 import br.com.sisnema.musica.repositories.AlbumRepository;
+import br.com.sisnema.musica.services.exceptions.RecursoNaoEncontrado;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -27,7 +30,9 @@ public class AlbumService {
     @Transactional(readOnly = true)
     public AlbumDto procurarPorId(Long id) {
         Optional<Album> objeto = repository.findById(id);
-        Album entidade = objeto.get();
+        //Album entidade = objeto.get();
+        Album entidade = objeto.orElseThrow(() ->
+                new RecursoNaoEncontrado("O ID " + id + " não existe. Camada de serviço."));
         return new AlbumDto(entidade);
     }
 
@@ -43,17 +48,26 @@ public class AlbumService {
 
     @Transactional
     public AlbumDto atualizar(Long id, AlbumDto dto) {
-        Album entidade = repository.getReferenceById(id);
-        entidade.setTitulo(dto.getTitulo());
-        entidade.setAno(dto.getAno());
-        entidade.setArtista(new Artista(dto.getArtista_id()));
-        entidade = repository.save(entidade);
-        return new AlbumDto(entidade);
+        try {
+            Album entidade = repository.getReferenceById(id);
+            entidade.setTitulo(dto.getTitulo());
+            entidade.setAno(dto.getAno());
+            entidade.setArtista(new Artista(dto.getArtista_id()));
+            entidade = repository.save(entidade);
+            return new AlbumDto(entidade);
+        }
+        catch (EntityNotFoundException e) {
+            throw new RecursoNaoEncontrado("Falha na atualização. O ID " + id + " não existe. Camada de serviço.");
+        }
     }
 
     public void excluir(Long id) {
-        repository.deleteById(id);
-        // No futuro vamos criar exceções.
+        try {
+            repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e) {
+            throw new RecursoNaoEncontrado("Falha na deleção. O ID " + id + " não existe. Camada de serviço.");
+        }
     }
 
 }
