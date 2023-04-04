1a8b8ed41cb58cea29d5ac653b9d189b43f0d2d2
Marcelo Gonçalves de Souza <mgoncalves@digitalhouse.com>
2023-04-04 19:47:21 -0300

    Update

--- 21-bancoSisnema/banco/src/test/java/br/com/sisnema/banco/factories/Factory.java
+++ 21-bancoSisnema/banco/src/test/java/br/com/sisnema/banco/factories/Factory.java
@@ -1,6 +1,7 @@
 package br.com.sisnema.banco.factories;
 
 import br.com.sisnema.banco.entities.Funcao;
+import br.com.sisnema.banco.entities.TipoConta;
 import br.com.sisnema.banco.entities.Usuario;
 
 public class Factory {
@@ -13,6 +14,11 @@ public class Factory {
     public static Usuario criarUsuario() {
         Usuario usuario = new Usuario(1L, "Felipe", "Massa", "massa@gmail.com", "123456");
         return usuario;
+    }
+
+    public static TipoConta criarTipoConta() {
+        TipoConta tipoConta = new TipoConta(1L, "Conjunta");
+        return tipoConta;
     }
 
 }
