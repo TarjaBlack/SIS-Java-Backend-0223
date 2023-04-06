160becefbd52c82e02b2501e81c4f504faf46325
Marcelo Gonçalves de Souza <mgoncalves@digitalhouse.com>
2023-04-05 21:09:03 -0300

    Update

---
+++ 23-SOLID/Solid/src/main/java/org/example/principioDaSubstituicaoDeLiskov/problema/CategoriaProblema.java
@@ -0,0 +1,4 @@
+package org.example.principioDaSubstituicaoDeLiskov.problema;
+
+public class CategoriaProblema {
+}
---
+++ 23-SOLID/Solid/src/main/java/org/example/principioDaSubstituicaoDeLiskov/solucao/Categoria.java
@@ -0,0 +1,4 @@
+package org.example.principioDaSubstituicaoDeLiskov.solucao;
+
+public class Categoria {
+}
