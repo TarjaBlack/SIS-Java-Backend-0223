3a81922b7ebc16a473b53b0d245d3ab41897e6e9
Marcelo Gonçalves de Souza <mgoncalves@digitalhouse.com>
2023-04-06 21:02:43 -0300

    Update

--- 21-bancoSisnema/banco/src/test/java/br/com/sisnema/banco/factories/Factory.java
+++ 21-bancoSisnema/banco/src/test/java/br/com/sisnema/banco/factories/Factory.java
@@ -1,6 +1,8 @@
 package br.com.sisnema.banco.factories;
 
 import br.com.sisnema.banco.dtos.FuncaoDto;
+import br.com.sisnema.banco.dtos.TipoContaDto;
+import br.com.sisnema.banco.dtos.UsuarioDto;
 import br.com.sisnema.banco.entities.Funcao;
 import br.com.sisnema.banco.entities.TipoConta;
 import br.com.sisnema.banco.entities.Usuario;
@@ -17,14 +19,23 @@ public class Factory {
         return new FuncaoDto(funcao);
     }
 
-    public static Usuario criarUsuario() {
-        Usuario usuario = new Usuario(1L, "Felipe", "Massa", "massa@gmail.com", "123456");
-        return usuario;
-    }
-
     public static TipoConta criarTipoConta() {
         TipoConta tipoConta = new TipoConta(1L, "Conjunta");
         return tipoConta;
     }
 
+    public static TipoContaDto criarTipoContaDto() {
+        TipoConta tipoConta = criarTipoConta();
+        return new TipoContaDto(tipoConta);
+    }
+
+    public static Usuario criarUsuario() {
+        Usuario usuario = new Usuario(1L, "Felipe", "Massa", "massa@gmail.com", "123456");
+        return usuario;
+    }
+
+    public static UsuarioDto criarUsuarioDto() {
+        Usuario usuario = criarUsuario();
+        return new UsuarioDto(usuario);
+    }
 }
